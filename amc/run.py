0f52ad7e9d1cc2081d5be013cd355f5d97f19887
Roland Wirth <roland.wirth@physik.tu-darmstadt.de>
2020-01-10 08:47:58 -0500

    backup commit for old command-line parser

--- amc/run.py
+++ amc/run.py
@@ -3,6 +3,7 @@ from __future__ import (division, absolute_import, print_function)
 
 import argparse
 
+
 def parse_command_line():
     """Return run commands from the Command Line Interface.
 
@@ -12,23 +13,23 @@ def parse_command_line():
     """
 
     parser = argparse.ArgumentParser(
-        description="ANGULAR MOMENTUM COUPLING v0.1\n\n"
-        + "Perform angular-momentum coupling on the given equations"
-        + "and return the result as a LaTeX file.")
+        description=
+        "ANGULAR MOMENTUM COUPLING v0.1\n\n"
+        "Perform angular-momentum coupling on the given equations"
+        "and return the result as a LaTeX file.")
 
     parser.add_argument('source', help='input file')
-    parser.add_argument('-b', '--binary', action='store_true', help='Input source file in binary format.')
     parser.add_argument('-o', '--output', nargs=1, help='output file')
     parser.add_argument('-p', '--permute', choices=['yes', 'no', 'smart'], default='smart',
-                      help='Permute tensor indices to find simpler formulas. `smart\' tries only the ones that are probable to succeed.')
-    parser.add_argument('--factorize_ninej', action='store_true', help='Factorize 9j-coefficients.')
-    parser.add_argument('--print_threej', action='store_true', help='Print 3j-coefficients.')
-    parser.add_argument('--select_equation', default=None,
-                      help='Select a specific equation (Use for debuging purpose).')
-    parser.add_argument('--select_term', default=None,
-                      help='Select a specific term of an equation (Use for debuging purpose).')
-    parser.add_argument('--select_permutation', default=None,
-                      help='Select a permutation of a term (Use for debuging purpose).')
+                      help="Permute tensor indices to find simpler formulas. `smart' tries only the ones that are probable to succeed.")
+    parser.add_argument('--collect-ninejs', action='store_true', help='Build 9j-coefficients from products of 6j-coefficients.')
+    parser.add_argument('--print-threejs', action='store_true', help='Print 3j-coefficients.')
+    parser.add_argument('--select-equation', default=None,
+                      help='Select a specific equation (Use for debugging purpose).')
+    parser.add_argument('--select-term', default=None,
+                      help='Select a specific term of an equation (Use for debugging purpose).')
+    parser.add_argument('--select-permutation', default=None,
+                      help='Select a permutation of a term (Use for debugging purpose).')
     parser.add_argument('-V', '--version', action='version', version='%(prog)s 0.1')
     parser.add_argument('-v', '--verbose', action='count', help='Increase verbosity', default=0)
 
