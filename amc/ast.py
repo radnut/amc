07ab7ec62a563dad5b2fec0ec1744471e8740cd7
Roland Wirth <roland.wirth@physik.tu-darmstadt.de>
2020-01-29 15:35:20 -0500

    GPLify the AMC package

--- amc/ast.py
+++ amc/ast.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2020  Julien Ripoche, Alexander Tichai, Roland Wirth
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """Abstract Syntax Tree classes.
 
 The ast module provides classes necessary to build abstract syntax trees for
